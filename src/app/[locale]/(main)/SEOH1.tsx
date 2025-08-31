b9499d19638da7d12f186eba563c5d4052de6efe
Aqib Ramzan <154926054+Aqib87Ramzan@users.noreply.github.com>
2025-08-31 23:12:54 +0500

    Update SEOH1.tsx

--- src/app/[locale]/(main)/SEOH1.tsx
+++ src/app/[locale]/(main)/SEOH1.tsx
@@ -1,7 +1,7 @@
 // app/[locale]/(main)/SEOH1.tsx
 export default function SEOH1() {
     return (
-      <h1 className="sr-only">
+      <h1 className="seo-only">
         SoCal Prime Homes – Luxury Homes Across Southern California
       </h1>
     )
